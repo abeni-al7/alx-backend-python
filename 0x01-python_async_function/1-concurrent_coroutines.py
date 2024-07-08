135dfa447c7821a599d62bc2a0a0cf3e6fbb9e19
abeni-al7 <abenezeralebachew3@gmail.com>
2024-07-09 02:49:10 +0300

    Fix linting

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,7 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''Returns the sorted waiting times of all calls'''
     coroutines = [wait_random(max_delay) for _ in range(n)]
