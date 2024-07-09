30d90e442b09c1c98538000b34b4584dedc8358f
abeni-al7 <abenezeralebachew3@gmail.com>
2024-07-09 03:06:54 +0300

    Add documentation and fix linting

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,6 +8,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
+    '''Returns the runtime of a coroutine'''
     t = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     s = time.perf_counter()
