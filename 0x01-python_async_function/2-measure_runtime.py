57f751a2f3f0e0a9e09da0a71b9d0ff8ffa871d1
abeni-al7 <abenezeralebachew3@gmail.com>
2024-07-09 03:03:39 +0300

    Measure the performance of a coroutine

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''A module for measuring the execution time of another function'''
+import time
+import asyncio
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    t = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    s = time.perf_counter()
+    return s - t
